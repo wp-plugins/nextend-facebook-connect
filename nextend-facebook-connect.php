9f15ca3bb62592b47ef8e8a6dc0ffe2b4992c9a4
nextendweb <nextendweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-05 09:40:38 +0000

    Added linking option to the profile page, so an already registered user can easily link the profile with a Facebook profile.
    git-svn-id: https://plugins.svn.wordpress.org/nextend-facebook-connect/trunk@608305 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nextend-facebook-connect.php
+++ nextend-facebook-connect.php
@@ -3,7 +3,7 @@
 Plugin Name: Nextend Facebook Connect
 Plugin URI: http://nextendweb.com/
 Description: This plugins helps you create Facebook login and register buttons. The login and register process only takes one click.
-Version: 1.2.2
+Version: 1.3.0
 Author: Roland Soos
 License: GPL2
 */
@@ -28,8 +28,26 @@ define( 'NEW_FB_LOGIN', 1 );
 if ( ! defined( 'NEW_FB_LOGIN_PLUGIN_BASENAME' ) )
 	define( 'NEW_FB_LOGIN_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
   
-register_activation_hook(__FILE__, 'nextend_fb_connect_install');
+/*
+  Sessions required for the profile notices 
+*/
+function new_fb_start_session() {
+  if(!session_id()) {
+      session_start();
+  }
+}
 
+function new_fb_end_session() {
+  session_destroy ();
+}
+
+add_action('init', 'new_fb_start_session', 1);
+add_action('wp_logout', 'new_fb_end_session');
+add_action('wp_login', 'new_fb_end_session');
+
+/*
+  Creating the required table on installation
+*/
 function nextend_fb_connect_install(){
   global $wpdb;
   
@@ -44,23 +62,22 @@ function nextend_fb_connect_install(){
 
    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
    dbDelta($sql);
- 
 }
+register_activation_hook(__FILE__, 'nextend_fb_connect_install');
 
-function add_author_more_query_var() {
-    global $wp;
-    $wp->add_query_var('author_more');
-}
-
-add_filter('init', 'new_fb_add_query_var');
-
+/*
+  Adding query vars for the WP parser
+*/
 function new_fb_add_query_var(){
   global $wp;
   $wp->add_query_var('loginFacebook');
   $wp->add_query_var('loginFacebookdoauth');
 }
+add_filter('init', 'new_fb_add_query_var');
 
-  
+/* -----------------------------------------------------------------------------
+  Main function to handle the Sign in/Register/Linking process
+----------------------------------------------------------------------------- */
 add_action('parse_request', new_fb_login);
 function new_fb_login(){
   global $wp, $wpdb;
@@ -69,7 +86,7 @@ function new_fb_login(){
     
     $user = $facebook->getUser();
     
-    if ($user && is_user_logged_in()) {
+    if ($user && is_user_logged_in() && new_fb_is_user_connected()) {
       header( 'Location: '.$_GET['redirect'] ) ;
       exit;
     }else{
@@ -89,32 +106,64 @@ function new_fb_login(){
         $ID = $wpdb->get_var($wpdb->prepare('
           SELECT ID FROM '.$wpdb->prefix.'social_users WHERE type = "fb" AND identifier = "'.$user_profile['id'].'"
         '));
-        if($ID == NULL){ // Register
-          $ID = email_exists($user_profile['email']);
-          if($ID == false){ // Real register
-            $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
-            $ID = wp_create_user( 'Facebook - '.$user_profile['name'], $random_password, $user_profile['email'] );
+        if(!is_user_logged_in()){
+          if($ID == NULL){ // Register
+            $ID = email_exists($user_profile['email']);
+            if($ID == false){ // Real register
+              $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
+              $ID = wp_create_user( 'Facebook - '.$user_profile['name'], $random_password, $user_profile['email'] );
+            }
+            $wpdb->insert( 
+            	$wpdb->prefix.'social_users', 
+            	array( 
+            		'ID' => $ID, 
+            		'type' => 'fb',
+                'identifier' => $user_profile['id']
+            	), 
+            	array( 
+            		'%d', 
+            		'%s',
+                '%s'
+            	) 
+            );
           }
-          $wpdb->insert( 
-          	$wpdb->prefix.'social_users', 
-          	array( 
-          		'ID' => $ID, 
-          		'type' => 'fb',
-              'identifier' => $user_profile['id']
-          	), 
-          	array( 
-          		'%d', 
-          		'%s',
-              '%s'
-          	) 
-          );
-        }
-        
-        if($ID){ // Login
-          wp_set_auth_cookie($ID, true, false);
-          header( 'Location: '.$_SESSION['redirect'] );
-          unset($_SESSION['redirect']);
-          exit;
+          
+          if($ID){ // Login
+            wp_set_auth_cookie($ID, true, false);
+            header( 'Location: '.$_SESSION['redirect'] );
+            unset($_SESSION['redirect']);
+            exit;
+          }
+        }else{
+          $current_user = wp_get_current_user();
+          if($current_user->ID == $ID){ // It was a simple login
+            header( 'Location: '.$_SESSION['redirect'] );
+            unset($_SESSION['redirect']);
+            exit;
+          }elseif($ID === NULL){  // Let's connect the accout to the current user!
+            $wpdb->insert( 
+            	$wpdb->prefix.'social_users', 
+            	array( 
+            		'ID' => $current_user->ID, 
+            		'type' => 'fb',
+                'identifier' => $user_profile['id']
+            	), 
+            	array( 
+            		'%d', 
+            		'%s',
+                '%s'
+            	) 
+            );
+            $_SESSION['new_fb_admin_notice'] = __('Your Facebook profile is successfully linked with your account. Now you can sign in with Facebook easily.', 'nextend-facebook-connect');
+            header( 'Location: '.$_SESSION['redirect'] );
+            unset($_SESSION['redirect']);
+            exit;
+          }else{
+            $_SESSION['new_fb_admin_notice'] = __('This Facebook profile is already linked with other account. Linking process failed!', 'nextend-facebook-connect');
+            header( 'Location: '.$_SESSION['redirect'] );
+            unset($_SESSION['redirect']);
+            exit;
+          }
         }
         exit;
       } catch (FacebookApiException $e) {
@@ -129,12 +178,56 @@ function new_fb_login(){
   }
 }
 
-function new_fb_login_url(){
-  return site_url('loginFacebook');
+/*
+  Is the current user connected the Facebook profile? 
+*/
+function new_fb_is_user_connected(){
+  global $wpdb;
+  $current_user = wp_get_current_user();
+  $ID = $wpdb->get_var($wpdb->prepare('
+    SELECT ID FROM '.$wpdb->prefix.'social_users WHERE type = "fb" AND ID = "'.$current_user->ID.'"
+  '));
+  if($ID === NULL) return false;
+  return true;
+}
+
+/*
+  Connect Field in the Profile page
+*/
+add_action('profile_personal_options', 'new_add_fb_connect_field');
+function new_add_fb_connect_field() {
+  global $new_is_social_header;
+  if($new_social_header === NULL){
+    ?>
+    <h3>Social connect</h3>
+    <?php
+    $new_social_header = true;
+  }
+  new_fb_is_user_connected();
+  ?>
+  <table class="form-table">
+    <tbody>
+      <tr>	
+        <th>
+          <label>Link Facebook with this profile</label>
+        </th>	
+        <td>
+          <?php if(!new_fb_is_user_connected()): ?>
+            <a href="<?php echo new_fb_login_url().'&redirect='.site_url().$_SERVER["REQUEST_URI"]; ?>">Link Facebook with this profile</a>
+          <?php else: ?>
+          Already connected
+          <?php endif; ?>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <?php
 }
 
 
-/* Options Page */
+/* 
+  Options Page 
+*/
 require_once(trailingslashit(dirname(__FILE__)) . "nextend-facebook-settings.php");
 
 if(class_exists('NextendFBSettings')) {
@@ -157,3 +250,24 @@ function nextend_fb_plugin_action_links( $links, $file ) {
 
 	return $links;
 }
+
+/* -----------------------------------------------------------------------------
+  Miscellaneous functions
+----------------------------------------------------------------------------- */
+function new_fb_login_url(){
+  return site_url('index.php').'?loginFacebook=1';
+}
+
+
+/*
+  Session notices used in the profile settings
+*/
+function new_fb_admin_notice(){
+  if(isset($_SESSION['new_fb_admin_notice'])){
+    echo '<div class="updated">
+       <p>'.$_SESSION['new_fb_admin_notice'].'</p>
+    </div>';
+    unset($_SESSION['new_fb_admin_notice']);
+  }
+}
+add_action('admin_notices', 'new_fb_admin_notice');
